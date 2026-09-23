01c18ba60b838ce8c32706deae70b862810cc45e
lavatech321 <37929186+lavatech321@users.noreply.github.com>
2026-09-23 09:19:59 +0530

    Delete sept-22 directory

--- sept-22/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "myterra-tfstate-original-bucket-1"
-    key          = "dev/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-  }
-}
-
